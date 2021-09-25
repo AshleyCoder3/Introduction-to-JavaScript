54deac59e402e436681e435223e83fd52bd8c893
Ashley Burns <Ash_burn999@yahoo.com>
2021-09-25 17:11:50 -0400

    task four complete

--- index.js
+++ index.js
@@ -44,31 +44,26 @@ function hungryDog(lbs, age){
    }
   }
 
-
-
-/*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
-
-// Rock, Paper, Scissors - Let's play against the computer!
-/*
-Create a global variable that randomly generates the computer's choice
-Use Math.random to determine the computers choice (Math.random gives a random number between 0 and 1)
-
-HINT: While you can complete this with only conditionals based on strings, it may help to equate choice to a number when using Math.random()
-
-Use the game function below to do the following:
-  1. Receive 2 parameters the user's choice and the computer's choice
-  2. Return whether the user won, lost, or tied based on these rules of the game described below - the strings returned need to match the strings below exactly.
-   - win should return "you win!"
-   - lose should return "you lose!"
-   - tie should return "it's a tie"
-  
-  RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
-  
-  HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
-*/
+let computer = Math.random()
+if(computer < .3){
+    computer ='scissors'
+} if( computer > .6){
+    computer = 'paper'
+} else {
+    computer = 'rock'
+}
 
 function game(user, computer){
-    /*add your code here*/
+
+    if (user === computer) {
+    return "it's a tie"
+    } else if (user === 'scissors' && computer === 'paper' ||
+    user === 'paper' && computer === 'rock'||
+    user === 'rock' && computer === 'scissors'){
+        return 'you win!'
+    } else{
+        return 'you lose!'
+    }
 }
   
   
