90bd881499ac971d7908b08c67a7c5e3b1a77a25
Ashley Burns <Ash_burn999@yahoo.com>
2021-09-26 11:24:29 -0400

    task seven complete

--- index.js
+++ index.js
@@ -78,24 +78,18 @@ function annoyingSong(number){
         return `${number} bottles of soda on the wall, ${number} bottles of soda, take one down pass it around ${number - 1} bottles of soda on the wall`
   }
 
-
-/*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
-
-//Grade Calculator
-/*
-Using the grade function below do the following: 
-  1. Receive a score out of 100 
-  2. Return the corresponding letter grade following this grade scale:
-
-   90-100 should return 'you got an A' 
-   80-89 should return 'you got a B'
-   70-79 should return 'you got a C'
-   60-69 should return 'you got a D'
-   below should return 'you got an F'
-*/
-  
-function grade(/*Your Code here */){
-  /*Your Code here */
+function grade(score){
+    if(score >= 90 && score <= 100){
+        return 'you got an A'
+    } else if (score >= 80 && score <= 89){
+        return 'you got a B'
+    } else if (score >= 70 && score <=79){
+        return 'you got a C'
+    } else if (score >= 60 && score <= 69){
+        return 'you got a D'
+    } else{
+         return 'you got an F'
+    }
   }
   
   
